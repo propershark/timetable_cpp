5c1ed06cbee6284af6a5589a6e3b25ac728edc76
Jon <jonegeland@gmail.com>
2017-02-25 13:53:44 -0500

    Start `visits_after` implementation This initial implementation takes in parameters and returns a list of departure times (no arrival times, yet) matching those parameters.
    Basic testing shows that the implementation is not accurate (visits are not returned in ascending time order), and it seems that the current structure of `visit_list` is partly at fault. The keying strategy currently keeps all visits on the same trip in ascending time order, but visits on the next trip may occur chronologically before them, resulting in an out-of-order result set, and thus incorrect results.

--- src/timetable.cc
+++ src/timetable.cc
@@ -8,46 +8,48 @@
 #include "transport.h"
 
 
-// MsgPackArr next_visits(std::string route, std::string stop_code, DateTime timestamp, int count) {
-//   return 1;
-// }
-// MsgPackArr next_visit(std::string arg1, std::string arg2, DateTime ts) { return next_visits(arg1, arg2, ts, 1); }
+Timetable::Timetable tt("data/citybus");
 
-// int last_visits(std::string arg1, std::string arg2, DateTime timestamp, int count) {
-//   return 1;
-// }
-// int last_visit(std::string arg1, std::string arg2, DateTime ts) { return last_visits(arg1, arg2, ts, 1); }
+MsgPack visits_after(std::string route_short_name, std::string stop_code, DateTime timestamp, int count) {
+  auto stop  = tt.stop_map[stop_code];
+  auto route = tt.routes_by_short_name[route_short_name];
+  auto start_time = timestamp.time;
+
+  std::vector<gtfs::stop_time> results;
+  for(auto pair : tt.visits_after({stop.id, route.id, "", start_time})) {
+    auto visit = pair.second;
+    if(!tt.is_active(visit, timestamp.date)) continue;
+
+    auto route_name     = tt.route_map[tt.trip_map[visit.trip_id].route_id].long_name;
+    auto arrival_time   = visit.arrival_time;
+    auto departure_time = visit.departure_time;
+    auto trip_id        = visit.trip_id;
+    std::cout << route_name << "  departs at  " << departure_time << "  on trip  " << trip_id << "\n";
+    results.push_back(visit);
+    if(results.size() >= count) break;
+  }
+
+  MsgPack payload;
+  payload.pack_array(results.size());
+  for(auto v : results) {
+    payload.pack(v.arrival_time);
+  }
+
+  return payload;
+}
+
+int visits_before(std::string arg1, std::string arg2, DateTime timestamp, int count) {
+  return 1;
+}
 
 
 
 int main() {
   Transport t("ws://shark-nyc1.transio.us:8080/ws");
 
-  Timetable::Timetable tt;
 
-  tt.add_visit({"Route 1", "Trip 1", "Stop001", "000"});
-  tt.add_visit({"Route 1", "Trip 1", "Stop003", "111"});
-  tt.add_visit({"Route 1", "Trip 1", "Stop004", "222"});
-  tt.add_visit({"Route 1", "Trip 1", "Stop004", "333"});
-  tt.add_visit({"Route 2", "Trip 1", "Stop001", "333"});
-  tt.add_visit({"Route 2", "Trip 1", "Stop003", "444"});
-  tt.add_visit({"Route 2", "Trip 1", "Stop001", "555"});
-  tt.add_visit({"Route 2", "Trip 1", "Stop002", "666"});
-  tt.add_visit({"Route 1", "Trip 2", "Stop002", "777"});
-  tt.add_visit({"Route 1", "Trip 1", "Stop002", "888"});
-
-  gtfs::stop::parser.parse("data/");
-  // auto iterator = tt.visits_after({"Route 1", "Trip 1", "888"});
-  // for(auto&& pair : iterator) {
-  //   auto visit = pair.second;
-  //   std::cout << visit.route_id << "-" << visit.trip_id << "-" << visit.departure << "\n";
-  // }
-  // Timetable::Timetable t("data/");
-
-  // t.procedure("timetable.next_visit",   next_visit);
-  // t.procedure("timetable.next_visits",  next_visits);
-  // t.procedure("timetable.last_visit",   last_visit);
-  // t.procedure("timetable.last_visits",  last_visits);
+  t.procedure("timetable.visits_after",   visits_after);
+  t.procedure("timetable.visits_before",  visits_before);
   t.start();
 
 
