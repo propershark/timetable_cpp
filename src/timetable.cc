c0cd438601fd2156a8703336cb20205d626e8891
Jon <jonegeland@gmail.com>
2017-03-16 20:49:58 -0500

    Expose new RPCs via `transport.procedure`. Until now, only the `visits_between*` RPCs were being registered with the WAMP router. Now all of the RPCs are properly registered and available for call.

--- src/timetable.cc
+++ src/timetable.cc
@@ -12,8 +12,12 @@ Timetable::Timetable tt("data/citybus");
 int main() {
   Transport t("ws://shark-nyc1.transio.us:8080/ws");
 
+  t.procedure("timetable.visits_before",              visits_before);
+  t.procedure("timetable.visits_before_from_route",   visits_before_from_route);
   t.procedure("timetable.visits_between",             visits_between);
   t.procedure("timetable.visits_between_from_route",  visits_between_from_route);
+  t.procedure("timetable.visits_after",               visits_after);
+  t.procedure("timetable.visits_after_from_route",    visits_after_from_route);
   t.start();
 
   // visits_between("BUS389", "20170130 06:44:00", "20170130 10:44:00", 2);
