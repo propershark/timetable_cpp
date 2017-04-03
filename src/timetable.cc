81f2b3956b89dc90c6e00a8dfa06b0b3d52c210e
Jon <jonegeland@gmail.com>
2017-04-03 00:36:04 -0400

    Fully initialize Timetable before registering RPCs. `create_indices()` is responsible for registering various indices with Timetable, which are then populated by `tt.initialize()`. These are generally required for RPCs to function properly, so they should be initialized before any RPCs are registered to avoid the possibility that an RPC is called before its indices are available.

--- src/timetable.cc
+++ src/timetable.cc
@@ -12,6 +12,9 @@ Timetable::Timetable tt("data/citybus");
 int main() {
   Transport t("ws://shark-nyc1.transio.us:8080/ws");
 
+  create_indices();
+  tt.initialize();
+
   t.procedure("timetable.visits_before",              visits_before);
   t.procedure("timetable.visits_before_from_route",   visits_before_from_route);
   t.procedure("timetable.visits_between",             visits_between);
@@ -19,9 +22,6 @@ int main() {
   t.procedure("timetable.visits_after",               visits_after);
   t.procedure("timetable.visits_after_from_route",    visits_after_from_route);
   t.start();
-
-  // visits_between("BUS389", "20170130 06:44:00", "20170130 10:44:00", 2);
-  // visits_between("BUS389", "20170130 06:44:00", "20170130 10:44:00", 10);
 
   return 0;
 }
